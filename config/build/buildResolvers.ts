fb6bc8262e1cd5dbab1eb01484770dcacc929338
Anna Pushkareva <smirnova_spb@mail.ru>
2023-05-08 11:00:49 +0300

    setting up css modules

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,5 +1,4 @@
-import { ResolveOptions } from "webpack";
-
+import {ResolveOptions} from "webpack";
 
 export function buildResolvers(): ResolveOptions {
     return {
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import './Counter.scss';
+import React, {useState} from 'react';
+import classes from './Counter.module.scss';
 
-export const Counter = () => {
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
     return (
-        <div>
-            <button>+</button>
+        <div className={classes.btn}>
+            <h1>{count}</h1>
+            <button onClick={increment}>increment</button>
         </div>
     );
 };
+
+export default Counter;
