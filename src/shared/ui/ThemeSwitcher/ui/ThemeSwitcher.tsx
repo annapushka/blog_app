e421a10f96ff8f34a4475139d67e0fac67398dd3
Anna Pushkareva <smirnova_spb@mail.ru>
2023-05-16 22:05:16 +0300

    add svgLoader, fileLoader

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,6 +1,8 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from './ThemeSwitcher.module.scss';
-import { useTheme } from "app/providers/ThemeProvider";
+import { Theme, useTheme } from "app/providers/ThemeProvider";
+import LightIcon from "shared/assets/icons/day-and-night-light.svg";
+import DarkIcon from "shared/assets/icons/day-and-night.svg";
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -10,12 +12,12 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
 
     return (
-            <button 
-                onClick={toggleTheme} 
-                className={classNames(cls.ThemeSwitcher, {}, [className])}
-            >
-                Toggle theme
-            </button>
+        <button
+            onClick={toggleTheme}
+            className={classNames(cls.ThemeSwitcher, {}, [className])}
+        >
+            {theme === Theme.DARK ? <LightIcon /> : <DarkIcon />}
+        </button>
     );
 };
 
