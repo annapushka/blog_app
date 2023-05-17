fa37a29670f4e681009769ad7d41305850f56dd6
Anna Pushkareva <smirnova_spb@mail.ru>
2023-05-17 22:08:44 +0300

    add Button UI kit

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,6 +3,7 @@ import cls from './ThemeSwitcher.module.scss';
 import { Theme, useTheme } from "app/providers/ThemeProvider";
 import LightIcon from "shared/assets/icons/day-and-night-light.svg";
 import DarkIcon from "shared/assets/icons/day-and-night.svg";
+import Button, { ThemeButton } from "shared/ui/Button/Button";
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -12,12 +13,13 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <button
+        <Button
+            theme={ThemeButton.CLEAR}
             onClick={toggleTheme}
             className={classNames(cls.ThemeSwitcher, {}, [className])}
         >
             {theme === Theme.DARK ? <LightIcon /> : <DarkIcon />}
-        </button>
+        </Button>
     );
 };
 
