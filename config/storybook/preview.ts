4c5cd9209af751b8aea66789642d2526161f7e4c
anna <anna.pushkareva@smddev.com>
2023-05-30 18:28:06 +0300

    add theme decorator

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,10 +1,13 @@
 import { Preview } from '@storybook/react';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider';
 
 export const preview: Preview = {
     parameters: {
         decorators: [
             StyleDecorator,
+            ThemeDecorator(Theme.LIGHT),
         ],
         actions: { argTypesRegex: '^on[A-Z].*' },
         controls: {
