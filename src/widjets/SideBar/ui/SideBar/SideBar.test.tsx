8fafeb157104a2ec8b021648a56dea710b8805e9
Anna Pushkareva <smirnova_spb@mail.ru>
2023-06-05 21:38:13 +0300

    fix tests

--- src/widjets/SideBar/ui/SideBar/SideBar.test.tsx
+++ src/widjets/SideBar/ui/SideBar/SideBar.test.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable i18next/no-literal-string */
 import { fireEvent, screen } from '@testing-library/react';
 import React from 'react';
+import { componentRender } from 'shared/config/tests/componentRender/componentRender';
 import { SideBar } from './SideBar';
-import { renderWithTranslation } from '../../../../shared/lib/tests/renderWithTranslation/renderWithTranslation';
 
 describe('SideBar', () => {
     test('render SideBar', () => {
-        renderWithTranslation(<SideBar />);
+        componentRender(<SideBar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('toggle', () => {
-        renderWithTranslation(<SideBar />);
+        componentRender(<SideBar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
