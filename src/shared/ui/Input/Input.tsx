b94bf26e4bbb21aa0bedee9f8dfdf65bed50647d
Anna Pushkareva <smirnova_spb@mail.ru>
2023-06-13 22:04:39 +0300

    add Input UI

---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,39 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { ChangeEvent, InputHTMLAttributes, memo } from 'react';
+import cls from './Input.module.scss';
+
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
+
+interface InputProps extends HTMLInputProps {
+    className?: string;
+    value?: string;
+    onChange?: (value: string) => void;
+    type?: string;
+}
+
+export const Input = memo((props: InputProps) => {
+    const {
+        className,
+        value,
+        onChange,
+        type = 'text',
+        ...otherProps
+    } = props;
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange?.(e.target.value);
+    };
+    return (
+        <div className={classNames(cls.Input, {}, [className])}>
+            <input
+                type={type}
+                value={value}
+                onChange={onChangeHandler}
+                className={cls.input}
+                {...otherProps}
+            />
+        </div>
+    );
+});
+
+export default Input;
