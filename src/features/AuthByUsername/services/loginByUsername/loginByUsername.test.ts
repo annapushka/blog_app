3c7b032b0125358a128e619617c3eab377eb6e3f
anna <anna.pushkareva@smddev.com>
2023-06-20 18:09:58 +0300

    add LoginState

---
+++ src/features/AuthByUsername/services/loginByUsername/loginByUsername.test.ts
@@ -0,0 +1,9 @@
+// import { createAsyncThunk } from "@reduxjs/toolkit"
+
+// const fetchUserById = createAsyncThunk(
+//     'users/fetchByIdStatus',
+//     async (userId: number, thunkAPI) => {
+//       const response = await userAPI.fetchById(userId)
+//       return response.data
+//     }
+//   )
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,15 +1,34 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import Button from 'shared/ui/Button/Button';
+import Button, { ButtonTheme } from 'shared/ui/Button/Button';
 import Input from 'shared/ui/Input/Input';
+import { useDispatch, useSelector } from 'react-redux';
+import { memo, useCallback } from 'react';
+import { loginActions } from '../../model/slice/loginSlice';
+import { getLoginState } from '../../model/selectors/getLoginState/getLoginState';
 import cls from './LoginForm.module.scss';
 
 interface LoginFormProps {
     className?: string;
 }
 
-export const LoginForm = ({ className }: LoginFormProps) => {
+export const LoginForm = memo(({ className }: LoginFormProps) => {
     const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const { username, password } = useSelector(getLoginState);
+
+    const onChangeUsername = useCallback((value: string) => {
+        dispatch(loginActions.setUsername(value));
+    }, [dispatch]);
+
+    const onChangePassword = useCallback((value: string) => {
+        dispatch(loginActions.setPassword(value));
+    }, [dispatch]);
+
+    const onLoginClick = useCallback(() => {
+        //
+    }, []);
+
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
             <Input
@@ -17,17 +36,25 @@ export const LoginForm = ({ className }: LoginFormProps) => {
                 className={cls.input}
                 placeholder={t('Введите имя пользователя')}
                 autofocus
+                onChange={onChangeUsername}
+                value={username}
             />
             <Input
                 type="text"
                 className={cls.input}
                 placeholder={t('Введите пароль')}
+                onChange={onChangePassword}
+                value={password}
             />
-            <Button className={cls.loginBtn}>
+            <Button
+                className={cls.loginBtn}
+                theme={ButtonTheme.OUTLINE}
+                onClick={onLoginClick}
+            >
                 {t('Войти')}
             </Button>
         </div>
     );
-};
+});
 
 export default LoginForm;
