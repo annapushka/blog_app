b859f8002ee4f23c4b041dc60cbf0145f93be550
Anna Pushkareva <smirnova_spb@mail.ru>
2023-07-13 22:10:41 +0300

    add profile reducer

--- src/entities/Profile/model/types/profile.ts
+++ src/entities/Profile/model/types/profile.ts
@@ -10,3 +10,10 @@ export interface Profile {
     username: string;
     avatar: string;
 }
+
+export interface ProfileSchema {
+    data?: Profile;
+    isLoading: boolean;
+    error?: string;
+    readonly: boolean;
+}
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,13 +1,20 @@
+import { profileReducer } from 'entities/Profile';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { DynamicModulLoader, ReducersList } from 'shared/lib/components/DynamicModulLoader/DynamicModulLoader';
 
+const reducers: ReducersList = {
+    profile: profileReducer,
+};
 interface ProfilePageProps {
     className?: string;
 }
 
 const ProfilePage = ({ className }: ProfilePageProps) => (
-    <div className={classNames('', {}, [className])}>
-        {1111}
-    </div>
+    <DynamicModulLoader reducers={reducers} removeAfterUnmount>
+        <div className={classNames('', {}, [className])}>
+            {1111}
+        </div>
+    </DynamicModulLoader>
 );
 
 export default ProfilePage;
