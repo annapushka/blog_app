c0ca9f21992c8a737b955268ec38123349e0d5c6
anna <anna.pushkareva@smddev.com>
2023-08-01 18:16:09 +0300

    43 13

---
+++ src/entities/Profile/model/services/fetchProfileData/fetchProfileData.test.ts
@@ -0,0 +1,46 @@
+import { Dispatch } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { userActions } from 'entities/User';
+import { TestAsyncThunk } from 'shared/config/tests/TestAsyncThunk/TestAsyncThunk';
+import { Currency } from 'entities/Currency';
+import { Country } from 'entities/Country';
+import { fetchProfileData } from './fetchProfileData';
+
+const data = {
+    first: 'Masha',
+    lastname: 'Yagoda',
+    age: 37,
+    currency: Currency.GBP,
+    country: Country.Azerbaijan,
+    city: 'Saint P',
+    username: 'admin',
+};
+
+describe('fetchProfileData.test', () => {
+    let dispatch: Dispatch;
+    let getState: () => StateSchema;
+
+    beforeEach(() => {
+        dispatch = jest.fn();
+        getState = jest.fn();
+    });
+
+    test('success', async () => {
+        const thunk = new TestAsyncThunk(fetchProfileData);
+        thunk.api.get.mockReturnValue(Promise.resolve({ data }));
+        const result = await thunk.callThunk();
+
+        expect(thunk.api.get).toHaveBeenCalled();
+        expect(result.meta.requestStatus).toBe('fulfilled');
+        expect(result.payload).toEqual(data);
+    });
+
+    test('unsuccess', async () => {
+        const thunk = new TestAsyncThunk(fetchProfileData);
+        thunk.api.post.mockReturnValue(Promise.resolve({
+            status: 403,
+        }));
+        const result = await thunk.callThunk();
+        expect(result.meta.requestStatus).toBe('rejected');
+    });
+});
