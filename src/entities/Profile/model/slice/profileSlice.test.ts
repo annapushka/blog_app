1212bb79f332dd6b0f94f6ceaef581c5df2c14e7
anna <anna.pushkareva@smddev.com>
2023-08-03 18:09:46 +0300

    fix tests

--- src/entities/Profile/model/slice/profileSlice.test.ts
+++ src/entities/Profile/model/slice/profileSlice.test.ts
@@ -1,7 +1,8 @@
 import { Currency } from 'entities/Currency';
 import { Country } from 'entities/Country';
-import { ProfileSchema } from '../types/profile';
+import { ProfileSchema, ValidateProfileError } from '../types/profile';
 import { profileActions, profileReducer } from './profileSlice';
+import { updateProfileData } from '../services/updateProfileData/updateProfileData';
 
 describe('profileSlice.test', () => {
     const data = {
@@ -46,6 +47,36 @@ describe('profileSlice.test', () => {
             form: {
                 username: 'Banana',
             },
+        });
+    });
+
+    test('test update profile service pending', () => {
+        const state: DeepPartial<ProfileSchema> = {
+            isLoading: false,
+            validateErrors: [ValidateProfileError.SERVER_ERROR],
+        };
+        expect(profileReducer(
+            state as ProfileSchema,
+            updateProfileData.pending,
+        )).toEqual({
+            isLoading: true,
+            validateErrors: undefined,
+        });
+    });
+
+    test('test update profile service fulfilled', () => {
+        const state: DeepPartial<ProfileSchema> = {
+            isLoading: true,
+        };
+        expect(profileReducer(
+            state as ProfileSchema,
+            updateProfileData.fulfilled(data, ''),
+        )).toEqual({
+            isLoading: false,
+            validateErrors: undefined,
+            readonly: true,
+            form: data,
+            data,
         });
     });
 });
