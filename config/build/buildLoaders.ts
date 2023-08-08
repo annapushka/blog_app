03cf4d98cd307ffa18a2539821a2b37e14229571
anna <anna.pushkareva@smddev.com>
2023-08-08 10:23:00 +0300

    add react-refresh-webpack-plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,30 +1,16 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/builCssLoader';
+import { builBabelLoader } from './loaders/builBabelLoader';
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     };
 
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [
-                    ['i18next-extract',
-                        {
-                            locales: ['ru', 'en'],
-                            keyAsDefaultValue: true,
-                        }],
-                ],
-            },
-        },
-    };
+    const babelLoader = builBabelLoader(options);
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
