caa7a1529e3bc728014c87d882fd15eee50b3232
anna <anna.pushkareva@smddev.com>
2023-08-16 10:01:03 +0300

    add articleDetails test

--- src/entities/Article/model/selectors/articleDetails.test.ts
+++ src/entities/Article/model/selectors/articleDetails.test.ts
@@ -1,18 +1,35 @@
 import { StateSchema } from 'app/providers/StoreProvider';
-import { getArticleDetailsData } from './articleDetails';
+import { getArticleDetailsData, getArticleDetailsError, getArticleDetailsIsLoading } from './articleDetails';
 
 describe('articleDetails.test', () => {
     test('should return data', () => {
+        const data = {
+            id: '1',
+            title: 'title',
+
+        };
         const state: DeepPartial<StateSchema> = {
             articleDetails: {
-                data: {
-                    id: '1',
-                    title: 'title',
-
-                },
+                data,
             },
         };
         expect(getArticleDetailsData(state as StateSchema)).toEqual(data);
+    });
+    test('should return isLoading', () => {
+        const state: DeepPartial<StateSchema> = {
+            articleDetails: {
+                isLoading: true,
+            },
+        };
+        expect(getArticleDetailsIsLoading(state as StateSchema)).toEqual(true);
+    });
+    test('should return error', () => {
+        const state: DeepPartial<StateSchema> = {
+            articleDetails: {
+                error: 'error',
+            },
+        };
+        expect(getArticleDetailsError(state as StateSchema)).toEqual('error');
     });
     test('should work with empty state', () => {
         const state: DeepPartial<StateSchema> = {};
