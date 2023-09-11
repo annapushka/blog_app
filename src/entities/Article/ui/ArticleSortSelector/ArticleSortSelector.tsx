2846f186a13be6afb7602d3d3e2791130271fd2c
anna <anna.pushkareva@smddev.com>
2023-09-11 18:05:16 +0300

    fix scss

--- src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
+++ src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
@@ -1,5 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { memo, useCallback, useMemo } from 'react';
+import { memo, useMemo } from 'react';
 
 import { useTranslation } from 'react-i18next';
 
