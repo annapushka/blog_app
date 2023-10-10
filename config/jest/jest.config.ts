d765faf1dea5fae359445cc086b39b4e633dec78
anna <anna.pushkareva@smddev.com>
2023-10-10 09:58:25 +0300

    add html reporter for unit tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -50,6 +50,15 @@ export default {
     setupFilesAfterEnv: [
         '<rootDir>config/jest/setupTests.ts',
     ],
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+            inlineSource: true,
+        }],
+    ],
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
