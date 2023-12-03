033c78041740f8bcb922f5473ba3429eb941e04c
Anna Pushkareva <smirnova_spb@mail.ru>
2023-12-03 11:13:33 +0300

    add article-details cy test 114 23

--- cypress/e2e/article/article-details.cy.ts
+++ cypress/e2e/article/article-details.cy.ts
@@ -1,4 +1,12 @@
 describe('template spec', () => {
+    beforeEach(() => {
+        cy.visit('');
+        cy.login();
+        cy.createArticle();
+    });
+    afterEach(() => {
+        cy.removeArticle();
+    });
     it('passes', () => {
         cy.visit('https://example.cypress.io');
     });
