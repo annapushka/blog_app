f7f346b99b258184ac15012f7a5fe844bb27f59b
anna <anna.pushkareva@smddev.com>
2023-12-19 17:31:18 +0300

    fix deps

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,32 +1,32 @@
 module.exports = {
-    stories: [
-        '../../src/**/*.stories.@(js|jsx|ts|tsx)',
-    ],
+  stories: [
+    '../../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
 
-    addons: [
-        '@storybook/addon-links',
-        {
-            name: '@storybook/addon-essentials',
-            options: {
-                backgrounds: false,
-            },
-        },
-        '@storybook/addon-interactions',
-        'storybook-addon-mock',
-        'storybook-addon-themes',
-    ],
-
-    framework: {
-        name: '@storybook/react-webpack5',
-        options: {},
+  addons: [
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+      },
     },
+    '@storybook/addon-interactions',
+    'storybook-addon-mock',
+    'storybook-addon-themes',
+  ],
 
-    staticDirs: ['../../src/shared/assets/tests/'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
 
-    docs: {
-        autodocs: true,
-    },
-    features: {
-        storyStoreV7: false,
-    },
+  staticDirs: ['../../src/shared/assets/tests/'],
+
+  docs: {
+    autodocs: true,
+  },
+  features: {
+    storyStoreV7: false,
+  },
 };
