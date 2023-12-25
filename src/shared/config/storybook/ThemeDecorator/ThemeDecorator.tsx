06a8ab6c1e0ea3b1348dbc798d77ebee8f0f67ed
anna <anna.pushkareva@smddev.com>
2023-12-25 18:27:16 +0300

    add setFeatureFlags method 125 05

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -3,10 +3,10 @@ import { Theme } from '@/shared/const/theme';
 // eslint-disable-next-line fsd-01/layer-imports
 import { ThemeProvider } from '@/app/providers/ThemeProvider';
 
-export const ThemeDecorator = (theme: Theme, Story: StoryFn) => () => (
+export const ThemeDecorator = (theme: Theme, Story?: StoryFn) => () => (
   <ThemeProvider initialTheme={theme ?? Theme.LIGHT}>
     <div className={`app ${theme ?? Theme.LIGHT}`}>
-      <Story />
+      {Story && <Story />}
     </div>
   </ThemeProvider>
 );
