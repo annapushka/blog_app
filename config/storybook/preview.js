b9ae1e80ce14e6caf0c85f47a9142263f5977e35
anna <anna.pushkareva@smddev.com>
2023-12-26 17:48:02 +0300

    add jsonSettings 127 3

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -5,26 +5,26 @@ import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDec
 import { Theme } from '../../src/shared/const/theme';
 
 export const parameters = {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
-        },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
-    layout: 'fullscreen',
-    decorators: [
-        (Story) => StyleDecorator(Story),
-        (Story) => ThemeDecorator(Theme.LIGHT, Story),
-        (Story) => RouterDecorator(Story),
-        (Story) => SuspenseDecorator(Story),
+  },
+  layout: 'fullscreen',
+  decorators: [
+    (Story) => StyleDecorator(Story),
+    (Story) => ThemeDecorator(Theme.LIGHT, Story),
+    (Story) => RouterDecorator(Story),
+    (Story) => SuspenseDecorator(Story),
+  ],
+  themes: {
+    default: 'dark',
+    list: [
+      { name: 'light', class: Theme.LIGHT, color: '#e2f8ff' },
+      { name: 'dark', class: Theme.DARK, color: '#2b2828' },
+      { name: 'blue', class: Theme.BLUE, color: '#5e69ee' },
     ],
-    themes: {
-        default: 'dark',
-        list: [
-            { name: 'light', class: Theme.LIGHT, color: '#e2f8ff' },
-            { name: 'dark', class: Theme.DARK, color: '#2b2828' },
-            { name: 'blue', class: Theme.BLUE, color: '#5e69ee' },
-        ],
-    },
+  },
 };
